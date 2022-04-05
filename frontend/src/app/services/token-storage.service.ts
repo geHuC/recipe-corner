53d598fa2517ea99a3979b5ca02f598832c77119
geHuC <79841526+geHuC@users.noreply.github.com>
2022-04-05 22:02:27 +0100

    created a tokenStorage service to hold the JWT token from the backend and user data

---
+++ frontend/src/app/services/token-storage.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+
+const USER_KEY = 'user';
+const TOKEN_KEY = 'token';
+
+let storedUser: any = {};
+let storedToken: string = '';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenStorageService {
+
+  constructor() { }
+  public saveToken(token: string): void {
+    window.localStorage.removeItem(TOKEN_KEY);
+    window.localStorage.setItem(TOKEN_KEY, token);
+    storedToken = token;
+  }
+  public saveUser(user: any): void {
+    window.localStorage.removeItem(USER_KEY);
+    window.localStorage.setItem(USER_KEY, JSON.stringify(user));
+    storedUser = { ...user };
+  }
+  public signOut(): void {
+    window.localStorage.removeItem(USER_KEY);
+    window.localStorage.removeItem(TOKEN_KEY);
+    storedToken = '';
+    storedUser = {};
+  }
+  public getToken(): string | null {
+    if (storedToken) return storedToken;
+    return window.localStorage.getItem(TOKEN_KEY);
+  }
+  public getUser(): any {
+    if (storedUser._id) return storedUser;
+    const user = window.sessionStorage.getItem(USER_KEY);
+    if (user) {
+      return JSON.parse(user);
+    }
+    return {};
+  }
+  public loadFromStorage(): void {
+    const user = window.sessionStorage.getItem(USER_KEY);
+    const token = window.localStorage.getItem(TOKEN_KEY);
+    if (user) {
+      storedToken = token || '';
+      storedUser = JSON.parse(user);
+    }
+  }
+}
+
