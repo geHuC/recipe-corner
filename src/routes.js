d6d97451c6ebf626a57e3c306b68920ec6fbb89a
geHuC <79841526+geHuC@users.noreply.github.com>
2022-04-06 22:17:34 +0100

    changed api auth route

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,8 @@ const submissionController = require('./controllers/submissionController.js');
 const authenticationController = require('./controllers/authenticationController.js');
 const userController = require('./controllers/userController.js');
 
+router.use('/api/v1/auth', authenticationController)
 router.use('/api/v1/submissions', submissionController);
 router.use('/api/v1/users', userController);
-router.use('/auth', authenticationController)
 
 module.exports = router;
