5cae8ab9c586ab1422e7f79c2bf3529429138dc5
geHuC <79841526+geHuC@users.noreply.github.com>
2022-04-06 22:18:51 +0100

    added isLogged() to auth service

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -2,19 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { shareReplay } from 'rxjs/operators'
 import { Observable } from 'rxjs';
+import { TokenStorageService } from './token-storage.service';
 
-const URL = 'http://localhost:3030';
+const URL = 'http://localhost:3030/api/v1/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private tokenStorage: TokenStorageService) { }
 
   login(username: string, password: string): Observable<any> {
-    return this.http.post(`${URL}/api/v1/auth`, { username, password }).pipe(shareReplay());
+    return this.http.post(`${URL}/login`, { username, password }).pipe(shareReplay());
   }
-  register(username: string, password: string, email: string) {
-    return this.http.post(`${URL}/api/v1/auth`, { username, password, email }).pipe(shareReplay());
+  register(username: string, email: string, password: string, repeatPassword: string, fullname: string): Observable<any> {
+    return this.http.post(`${URL}/register`, { username, email, password, repeatPassword, fullname }).pipe(shareReplay());
+  }
+  isLogged(): boolean {
+    return this.tokenStorage.getUser()._id ? true : false;
   }
 }
