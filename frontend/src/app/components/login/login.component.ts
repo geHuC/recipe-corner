3b4210ec71d8e2a46e6b5f30dcd974167ae65a43
geHuC <79841526+geHuC@users.noreply.github.com>
2022-04-06 22:20:08 +0100

    basic login form

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginFailed = false;
+  erroMessage = '';
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
+
+  ngOnInit(): void {
+  }
+  onSubmit(login: any): void {
+    const { username, password } = login.form.value;
+    this.authService.login(username, password).subscribe(
+      data => {
+        this.tokenStorage.saveToken(data.token);
+        this.tokenStorage.saveUser(data.user);
+      }
+    )
+  }
+
+}
