e66a2abe97a7772015ef44e8e9c0f9e41d92976d
geHuC <79841526+geHuC@users.noreply.github.com>
2022-04-09 13:55:39 +0100

    Created recipe card component for homepage use

---
+++ frontend/src/app/components/recipe-card/recipe-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { IRecipeMini } from 'src/app/interfaces/irecipe-mini';
+
+@Component({
+  selector: 'app-recipe-card',
+  templateUrl: './recipe-card.component.html',
+  styleUrls: ['./recipe-card.component.css']
+})
+export class RecipeCardComponent implements OnInit {
+  @Input() recipe!: IRecipeMini;
+  recipeUrl:string = '';
+  stringify:string = JSON.stringify(this.recipe);
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.recipeUrl = `/recipe/${this.recipe.author.username}/${this.recipe.slug}`;
+  }
+
+}
