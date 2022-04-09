9e7dde23f28d092f1fba62f8d90fa9987f528984
geHuC <79841526+geHuC@users.noreply.github.com>
2022-04-09 21:28:41 +0100

    added some nav links to theader and made search bar work

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
 
+  constructor(private auth: AuthService, public storage: TokenStorageService, private router: Router) { }
   ngOnInit(): void {
   }
-
+  logout(): void {
+    this.auth.logout();
+  }
+  onSearch(search: any): void {
+    const searchTerm = search.value['search-term'];
+    if (searchTerm != '') this.router.navigate(['search'], { queryParams: { q: searchTerm } })
+  }
 }
