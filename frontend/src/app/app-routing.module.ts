897e87dc1216b4aa77a1d92cfc9290fc6bd70c36
geHuC <79841526+geHuC@users.noreply.github.com>
2022-04-09 13:58:34 +0100

    added more routing paths

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './components/create/create.component';
+import { DetailsPageComponent } from './components/details-page/details-page.component';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AuthGuard } from './guards/auth.guard';
 import { GuestGuard } from './guards/guest.guard';
 
 const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
-    canActivate: [GuestGuard]
-  }
-
+  { path: '', component: HomeComponent, },
+  { path: 'login', component: LoginComponent, canActivate: [GuestGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [GuestGuard] },
+  { path: 'create', component: CreateComponent, canActivate: [AuthGuard] },
+  { path: 'recipe/:author/:slug', component: DetailsPageComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
