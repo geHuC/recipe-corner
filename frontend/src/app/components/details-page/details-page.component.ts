b2a5ddf07436be17576da804e155139d54c8ed14
geHuC <79841526+geHuC@users.noreply.github.com>
2022-04-09 13:57:04 +0100

    Created a details page that takes data from either state or if no state pulls from the backend

---
+++ frontend/src/app/components/details-page/details-page.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IRecipeMini } from 'src/app/interfaces/irecipe-mini';
+import { SubmissionService } from 'src/app/services/submission.service';
+
+@Component({
+  selector: 'app-details-page',
+  templateUrl: './details-page.component.html',
+  styleUrls: ['./details-page.component.css']
+})
+export class DetailsPageComponent implements OnInit {
+  recipe!: IRecipeMini;
+  private author!: string;
+  private slug!: string;
+
+  constructor(private route: ActivatedRoute, private ss: SubmissionService) { }
+
+  ngOnInit(): void {
+    this.author = this.route.snapshot.paramMap.get('author') || '';
+    this.slug = this.route.snapshot.paramMap.get('slug') || '';
+
+    let state = history.state;
+    if (state._id) {
+      this.recipe = state;
+      console.log('State: ', this.recipe);
+      
+    } else {
+      this.ss.getSingle(this.author, this.slug).subscribe(
+        data => {this.recipe = data; console.log(data);
+        }
+      )
+      console.log('From db');
+    }
+  }
+
+}
