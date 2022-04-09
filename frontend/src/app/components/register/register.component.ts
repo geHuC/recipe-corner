62aaaf77c8d671317a976850b33ebcea4ed5ecf7
geHuC <79841526+geHuC@users.noreply.github.com>
2022-04-09 13:57:52 +0100

    Added navigation to homepage after registration

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +9,7 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private tokenStorage:TokenStorageService, private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -17,7 +19,9 @@ export class RegisterComponent implements OnInit {
 
     this.authService.register(username, email, password, repeatPassword, fullname).subscribe(
       data => {
-        console.log(data);
+        this.tokenStorage.saveUser(data.user);
+        this.tokenStorage.saveToken(data.token);
+        this.router.navigate(['']);
       }
     )
   }
