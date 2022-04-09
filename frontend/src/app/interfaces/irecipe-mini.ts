cba91e8f3ebe758b9ca16bec3b8d784fdd7c1b1c
geHuC <79841526+geHuC@users.noreply.github.com>
2022-04-09 13:55:52 +0100

    Created a recipe interface

---
+++ frontend/src/app/interfaces/irecipe-mini.ts
@@ -0,0 +1,15 @@
+export interface IRecipeMini {
+    _id: string;
+    title: string;
+    imageUrl: string;
+    thumbUrl: string;
+    ingredients: any;
+    author: any;
+    steps: any;
+    description: string;
+    favourites: any;
+    createdAt: string;
+    updatedAt: string;
+    slug:string;
+    __v: number;
+}
