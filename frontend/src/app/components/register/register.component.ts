e000f26b0717135038d558e877cb6f8c82b8621f
geHuC <79841526+geHuC@users.noreply.github.com>
2022-04-09 21:27:00 +0100

    Set up for completing input validation on register form

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import ConfirmValidators from 'src/app/helpers/confirmValidator';
 import { AuthService } from 'src/app/services/auth.service';
 import { TokenStorageService } from 'src/app/services/token-storage.service';
 
@@ -9,21 +11,41 @@ import { TokenStorageService } from 'src/app/services/token-storage.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  constructor(private authService: AuthService, private tokenStorage:TokenStorageService, private router:Router) { }
+  form!: FormGroup;
 
-  ngOnInit(): void {
-  }
-  onSubmit(register:any): void {
-    const { username, email, password, repeatPassword, fullname } = register.form.value;
-    console.log(username);
-
-    this.authService.register(username, email, password, repeatPassword, fullname).subscribe(
-      data => {
-        this.tokenStorage.saveUser(data.user);
-        this.tokenStorage.saveToken(data.token);
-        this.router.navigate(['']);
-      }
+  constructor(private authService: AuthService,
+    private tokenStorage: TokenStorageService,
+    private router: Router,
+    private fb: FormBuilder) {
+  };
+  ngOnInit() {
+    this.form = this.fb.group(
+      {
+        username: ['', [Validators.required, Validators.minLength(5), Validators.pattern(/^[a-zA-Z0-9_]+$/)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        repeatPassword: ['', [Validators.required]]
+      }, {
+      validators: [ConfirmValidators.MustMatch('password', 'repeatPassword')]
+    }
     )
+  };
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
   }
+  onSubmit(): void {
+    console.log('fuck angular');
+    console.log(this.form);
+    
 
+    // const { username, email, password, repeatPassword } = register.form.value;
+
+    // this.authService.register(username, email, password).subscribe(
+    //   data => {
+    //     this.tokenStorage.saveUser(data.user);
+    //     this.tokenStorage.saveToken(data.token);
+    //     this.router.navigate(['']);
+    //   }
+    // )
+  }
 }
