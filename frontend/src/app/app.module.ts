4a84e3dbf2f638243c798c97f07da84a1751cadf
geHuC <79841526+geHuC@users.noreply.github.com>
2022-04-11 20:21:06 +0100

    Misc imports to appmodule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,18 +12,31 @@ import { AuthGuard } from './guards/auth.guard';
 import { GuestGuard } from './guards/guest.guard';
 import { authInterceptorProviders } from './helpers/auth.interceptor';
 import { TokenStorageService } from './services/token-storage.service';
+import { CreateComponent } from './components/create/create.component';
+import { HomeComponent } from './components/home/home.component';
+import { RecipeCardComponent } from './components/recipe-card/recipe-card.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { DetailsPageComponent } from './components/details-page/details-page.component';
+import { SearchPageComponent } from './components/search-page/search-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    CreateComponent,
+    HomeComponent,
+    RecipeCardComponent,
+    NotFoundComponent,
+    DetailsPageComponent,
+    SearchPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
   ],
   providers: [authInterceptorProviders, TokenStorageService, AuthGuard, GuestGuard],
