d85c23c464a6aa2f5cc4a8394c66c4b19acb850a
geHuC <79841526+geHuC@users.noreply.github.com>
2022-04-16 02:13:21 +0100

    test deploy

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 //Require packages
 const express = require('express');
 require('dotenv').config();
+const path = require('path');
 const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3030;
-const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION_MD;
 const routes = require('./routes')
 
 //Setup configurations
@@ -14,6 +15,7 @@ const mongoDbConnection = require('./configurations/mongooseConfig'); //mongoose
 const { urlencoded } = require('express');
 
 //Setup the router
+app.use(express.static(path.join(__dirname,"build")));
 app.use(cors());
 app.use(routes);
 app.use(urlencoded);
@@ -21,7 +23,7 @@ app.use(urlencoded);
 //     res.status(404);
 // });
 //Start sequence
-//Making sure we have a DB connection before letting anyone interact with the service
+//Making sure we have a DB connection before letting anyone interact with the service 
 mongoDbConnection(DB_CONNECTION_STRING).then(() => {
     console.log(`Connected to DB at`, DB_CONNECTION_STRING);
     //Start the server on DB connection
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router(); //Direclty generate a router
+const path = require('path');
 const submissionController = require('./controllers/submissionController.js');
 const authenticationController = require('./controllers/authenticationController.js');
 const userController = require('./controllers/userController.js');
@@ -6,5 +7,8 @@ const userController = require('./controllers/userController.js');
 router.use('/api/v1/auth', authenticationController)
 router.use('/api/v1/submissions', submissionController);
 router.use('/api/v1/users', userController);
+router.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'build','index.html'))
+})
 
 module.exports = router;
